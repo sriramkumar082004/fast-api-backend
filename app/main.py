e04fb842e46f2eb8a6cc7b922ab6eb8c01309ca0
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-31 15:05:01 +0530

    Fix: add rootDir to render.yaml

--- app/main.py
+++ app/main.py
@@ -5,13 +5,18 @@ from app.routers import auth, students, utils
 
 app = FastAPI(title="FastAPI Backend")
 
+origins = [
+    "http://localhost:5173",
+    "https://react-api-frontend-weld.vercel.app",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=origins, # Allows all origins for testing. Replace with specific domains in production.
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth.router, prefix=f"{settings.API_V1_STR}/auth", tags=["auth"])
 app.include_router(
     students.router, prefix=f"{settings.API_V1_STR}/students", tags=["students"]
